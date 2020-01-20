f2f0b05ae734b08c1aa737c605e9a4d0e0e9551d
Alexander Diemand <codieplusplus@apax.net>
2020-01-20 21:50:44 +0100

    try 1
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- src/System/Remote/Monitoring/Prometheus.hs
+++ src/System/Remote/Monitoring/Prometheus.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -17,8 +18,9 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.Trans.State.Strict
 import qualified Data.HashMap.Strict as HMap
-import qualified Data.Map.Strict as Map
+--import qualified Data.Map.Strict as Map
 import           Data.Monoid
+import           Data.Hashable
 import Lens.Micro.TH
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -45,7 +47,10 @@ data Metric =
     C Counter.Counter
   | G Gauge.Gauge
 
-type MetricsMap = Map.Map Prometheus.Name Metric
+type MetricsMap = HMap.HashMap Prometheus.Name Metric
+
+instance Hashable Prometheus.Name where
+  hashWithSalt s n = hashWithSalt s (Prometheus.unName n)
 
 --------------------------------------------------------------------------------
 defaultOptions :: Prometheus.Labels -> AdapterOptions
@@ -55,12 +60,12 @@ defaultOptions l = AdapterOptions l Nothing 15
 registerEKGStore :: MonadIO m => EKG.Store -> AdapterOptions -> RegistryT m ()
 registerEKGStore store opts = RegistryT $ StateT $ \_ -> do
   (r, mmap) <- liftIO $ toPrometheusRegistry' store opts
-  liftIO $ forkIO $ do
+  liftIO $ forkIO $
     let loop = forever $ do
                  threadDelay (_samplingFrequency opts * 10^6)
                  updateMetrics store opts mmap
-                 loop
-    loop
+                 --loop
+    in loop
   return ((), r)
 
 --------------------------------------------------------------------------------
@@ -68,7 +73,8 @@ toPrometheusRegistry' :: EKG.Store -> AdapterOptions -> IO (Prometheus.Registry,
 toPrometheusRegistry' store opts = do
   let registry = Prometheus.new
   samples <- EKG.sampleAll store
-  foldM (mkMetric opts) (registry, Map.empty) (HMap.toList samples)
+  let !sps = HMap.toList samples
+  foldM (mkMetric opts) (registry, HMap.empty) sps
 
 --------------------------------------------------------------------------------
 toPrometheusRegistry :: EKG.Store -> AdapterOptions -> IO Prometheus.Registry
@@ -82,11 +88,11 @@ mkMetric AdapterOptions{..} (oldRegistry, mmap) (key, value) = do
    EKG.Counter c -> do
      (counter, newRegistry) <- Prometheus.registerCounter k _labels oldRegistry
      Counter.add (fromIntegral c) counter
-     return $! (newRegistry, Map.insert k (C counter) $! mmap)
+     return $! (newRegistry, HMap.insert k (C counter) $! mmap)
    EKG.Gauge g   -> do
      (gauge, newRegistry) <- Prometheus.registerGauge k _labels oldRegistry
      Gauge.set (fromIntegral g) gauge
-     return $! (newRegistry, Map.insert k (G gauge) $! mmap)
+     return $! (newRegistry, HMap.insert k (G gauge) $! mmap)
    EKG.Label _ -> return $! (oldRegistry, mmap)
    EKG.Distribution _ -> return $! (oldRegistry, mmap)
 
@@ -94,7 +100,9 @@ mkMetric AdapterOptions{..} (oldRegistry, mmap) (key, value) = do
 updateMetrics :: EKG.Store -> AdapterOptions -> MetricsMap -> IO ()
 updateMetrics store opts mmap = do
   samples <- EKG.sampleAll store
-  const () <$> foldM (updateMetric opts) mmap (HMap.toList samples)
+  --const () <$> foldM (updateMetric opts) mmap (HMap.toList samples)
+  let !sps = HMap.toList samples
+  foldM_ (updateMetric opts) mmap sps
 
 
 --------------------------------------------------------------------------------
@@ -106,7 +114,7 @@ mkKey mbNs k =
 updateMetric :: AdapterOptions -> MetricsMap -> (T.Text, EKG.Value) -> IO MetricsMap
 updateMetric AdapterOptions{..} mmap (key, value) = do
   let k = mkKey _namespace key
-  case liftM2 (,) (Map.lookup k mmap) (Just value) of
+  case liftM2 (,) (HMap.lookup k mmap) (Just value) of
     Just (C counter, EKG.Counter c)  -> do
       (Counter.CounterSample oldCounterValue) <- Counter.sample counter
       let slack = c - fromIntegral oldCounterValue
